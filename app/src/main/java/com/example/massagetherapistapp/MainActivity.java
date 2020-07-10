0297c5af495aacc3d4fafd4e829d7ce58a0d0b37
user <teezx-wm19@student.tarc.edu.my>
2020-07-10 15:21:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/massagetherapistapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.massagetherapistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //this is the assignment
+        //test
+    }
+}
