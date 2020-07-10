2b017d92f0d0f3e2b33ccde797404b7139515987
limxy-wm19 <limxy-wm19@student.tarc.edu.my>
2020-07-10 15:49:34 +0800

    commit by lim

---
+++ app/src/main/java/com/example/massagetherapistapp/Product_Service.java
@@ -0,0 +1,14 @@
+package com.example.massagetherapistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Product_Service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product__service);
+    }
+}
